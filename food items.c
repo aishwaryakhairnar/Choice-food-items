4e185fa9871efb6a4bcf75f3b9bed1cc7dabce07
aishwaryakhairnar <92526650+aishwaryakhairnar@users.noreply.github.com>
2021-10-16 20:03:13 +0530

    Update food items.c

--- food items.c
+++ food items.c
@@ -1,4 +1,4 @@
-.h>
+#include<stdio.h>
 main()
 {
 	printf("Enter a number:\n1.\n2.\n3.\n4.\n5.");
@@ -7,21 +7,21 @@ main()
 	
 	switch(choice)
 	{
-		 case 1:
-			printf("Food item-French Fries.\n Price-Rs99");
-			break;
+	   case 1:
+	        printf("Food item-French Fries.\n Price-Rs99");
+		break;
 	   case 2:
 	    	printf("Food item-Burger.\n Price-Rs129");
 	    	break;
 	   case 3:	
-		    printf("Food item-Sandwich,.\n Price-Rs149");
+	        printf("Food item-Sandwich,.\n Price-Rs149");
 	        break;
 	   case 4:	
-		    printf("Food item-Pasta.\n Price-Rs179");
-		    break;
+		printf("Food item-Pasta.\n Price-Rs179");
+		break;
 	   case 5:	
-		    printf("Food item-Pizza.\n Price-Rs239");
-		    break;
-		default : printf("Invalid choice");
+		printf("Food item-Pizza.\n Price-Rs239");
+		break;
+	   default : printf("Invalid choice");
 	}
 }
